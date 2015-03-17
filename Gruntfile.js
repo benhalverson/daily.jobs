ff8edf19f544c0151887cdcc5038907df8650521
Ben Halverson <benhalverson33@gmail.com>
2015-03-17 02:00:28 -0700

    setting up grunt build system

---
+++ Gruntfile.js
@@ -0,0 +1,29 @@
+module.exports = function(grunt) {
+
+
+	grunt.initConfig({
+		concat: {
+		js: {
+			src: ["js/js1.js", "js/js2.js"],
+			dest: "build/js/scripts.js"
+			},
+			css: {
+				src: ["css/main.css", "css/theme.css"],
+				dest: "build/css/styles.css"
+			}
+		},
+		watch: {
+			js: {
+				files: ["js/**/*.js"],
+				tasks: ["concat:js"]
+			},
+			css: {
+				files: ["css/**/*.css"],
+				tasks: ["concat:css"]
+			}
+		}
+	});
+	grunt.loadNpmTasks("grunt-contrib-concat");
+	grunt.loadNpmTasks("grunt-contrib-watch");
+	grunt.registerTask("default", ["concat", "watch" ]);
+};
---
+++ build/js/scripts.js
@@ -0,0 +1,3 @@
+//console.log("hello from js1.js");
+
+console.log("hello from js2.js");
